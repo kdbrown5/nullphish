ccd1e381a7d96de07009a46857db84e7edf591a7
kdbrown5 <46332316+kdbrown5@users.noreply.github.com>
2020-01-15 22:23:51 -0800

    == instead of =

--- lumberjack.py
+++ lumberjack.py
@@ -17,15 +17,15 @@ def log(inx):
     #Convert certain variables to 2-characters if needed
     if len(yr)>2:
         yr = yr[-2]
-    if len(mt)=1:
+    if len(mt)==1:
         mt = "0" + mt
-    if len(dy)=1:
+    if len(dy)==1:
         dy = "0" + dy
-    if len(hr)=1:
+    if len(hr)==1:
         hr = "0" + hr
-    if len(mn)=1:
+    if len(mn)==1:
         mn = "0" + mn
-    if len(sc)=1:
+    if len(sc)==1:
         sc = "0" + sc
 
     #Finalize entry beginning
