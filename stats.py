76edff0d645c3399a5a0b3492919ae49bbc95642
Kevin <kdbrown5@gmail.com>
2020-01-22 20:45:21 -0800

    Stats query and deletion of records

--- stats.py
+++ stats.py
@@ -36,14 +36,53 @@ def stat():
             firstname = []
             lastname = []
             for row in cur.execute('select DISTINCT firstname from tests where business LIKE (?);', (business,)):
+                row = str(row).replace('(', '')
+                row = str(row).replace(')', '')
+                row = str(row).replace(',', '')
+                row = str(row).replace("'", '')
                 firstname.append(row[:])
             for row in cur.execute('select DISTINCT lastname from tests where business LIKE (?);', (business,)):
+                row = str(row).replace('(', '')
+                row = str(row).replace(')', '')
+                row = str(row).replace(',', '')
+                row = str(row).replace("'", '')
                 lastname.append(row[:])            
         con.close()
-        firstname = str(firstname)
         return firstname, lastname
+
+    def modifyuser(searchfirst, searchlast):
+        con = sqlite3.connect('static/db1.db')
+        with con:
+            cur = con.cursor()
+            cur.execute('select * from tests where firstname LIKE (?) and lastname LIKE (?);', (searchfirst, searchlast),)
+            usertests = cur.fetchall()
+        con.close()
+        return usertests
+
+    def deleterecord(record):
+        con = sqlite3.connect('static/db1.db')
+        with con:
+            cur = con.cursor()
+            cur.execute('delete from tests where id = (?);', (record),)
+        con.close()
+        return render_template('stats.html', businessdata=businessdata, firstlast=firstlast)
+
     businessdata = businesslookup()
     firstname, lastname = userlookup()
+    firstlast = list(map(list, zip(firstname, lastname)))
+
+    if 'modifyuser' in request.form:
+        firstlast1 = request.form['modifyuser']
+        firstlast1 = firstlast1.split(',')
+        searchfirst = firstlast1[0][2:-1]
+        searchlast = firstlast1[1][2:-2]
+        usertests = modifyuser(searchfirst, searchlast)
+        return render_template('stats.html', businessdata=businessdata, usertests=usertests)
+
+    if 'deleterecord' in request.form:
+        record = (request.form['deleterecord'][1])
+        deleterecord(record)
+        return render_template('stats.html', businessdata=businessdata, firstlast=firstlast)
 
 
     if 'main' in request.form:
@@ -54,4 +93,4 @@ def stat():
         return redirect("/stats")
 
 
-    return render_template('stats.html', businessdata=businessdata)
+    return render_template('stats.html', businessdata=businessdata, firstlast=firstlast)
