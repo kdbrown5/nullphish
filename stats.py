b45721a526a39a7344e2ee79a13d30f4b327bbf4
Kevin <kdbrown5@gmail.com>
2020-01-25 00:04:45 -0800

    clean up and comments

--- stats.py
+++ stats.py
@@ -7,10 +7,9 @@ from flask_sqlalchemy import SQLAlchemy
 import sqlite3 as sql
 from lumberjack import log
 
-
 stats = Blueprint('stats', __name__, url_prefix='/stats', template_folder='templates')
 
-@stats.route('/stats/mod', methods=['GET', 'POST'])
+@stats.route('/stats/mod', methods=['GET', 'POST']) ### url to keep modification / record deletion open
 def waitforrecdel1():
     def businesslookup():
         con = sqlite3.connect('static/db1.db')
@@ -20,13 +19,13 @@ def waitforrecdel1():
         with con:
             cur = con.cursor()
             businessquery = []
-            for row in cur.execute('select DISTINCT * from tests where business LIKE (?) and notify = 1;', (business,)):
+            for row in cur.execute('select DISTINCT * from tests where business LIKE (?) and notify = 1;', (business,)):## populate tables with user data from same business
                 businessquery.append(row[:])
         con.close()
         businessdata = businessquery
         return businessquery
         
-    businessdata = businesslookup()
+    businessdata = businesslookup()# return userdata list to render on page
 
     if 'exitmodify' in request.form:
         return redirect('/stats')
@@ -54,26 +53,25 @@ def waitforrecdel():
         con = sqlite3.connect('static/db1.db')
         with con:
             cur = con.cursor()
-            cur.execute('select business from tests where id = (?);', (delrec,))
+            cur.execute('select business from tests where id = (?);', (delrec,))# grab business name from test deletion request
             businessverify = cur.fetchone()
-            if str(businessverify[0]) == str(session['business'][1:-1]):
-                cur.execute('update tests set notify = 0 where id = (?);', (delrec,))
+            if str(businessverify[0]) == str(session['business'][1:-1]):# compare test business name to admin user business name
+                cur.execute('update tests set notify = 0 where id = (?);', (delrec,))# delete record
             else:
-                flash('Why are you trying to hack the gibson?', 'category2')
+                flash('Why are you trying to hack the gibson?', 'category2')# someone is haxing us
 
     businessdata = businesslookup()
 
     if request.method == 'GET':
-        print(request.args.get('rec'[:]))
-        if request.args.get('rec'[:]) == None:
+        if request.args.get('rec'[:]) == None:# if no record deletion requested
             return render_template('stats-modify.html', businessdata=businessdata)
         else:
-            delrec = request.args.get('rec')
+            delrec = request.args.get('rec')# record deletion requested - ie - nullphish.com/stats/del?rec=5 as example
             deleterecord(delrec)
-            return redirect('/stats/mod')
+            return redirect('/stats/mod')# return to table w/ modification enabled render
 
     if 'exitmodify' in request.form:
-        return redirect('/stats')
+        return redirect('/stats')# return to locked tables
 
     return render_template('stats-modify.html', businessdata=businessdata)    
 
