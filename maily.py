3c8c9984782265cc07ac1cc89cbf63ecd31fcbc5
Kevin <kdbrown5@gmail.com>
2020-01-31 18:15:38 -0800

    Script to send templated email out to users

---
+++ maily.py
@@ -0,0 +1,42 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from flask import Flask, flash, session, render_template, render_template_string, request, jsonify, redirect, url_for, \
+    Response, g, Markup, Blueprint, make_response
+from bs4 import BeautifulSoup
+
+def sendphish(inserttemplate, receiveremail):
+    sender_email = "donotreply@nullphish.com"
+    receiver_email = receiveremail
+    password = "rtatstfu18as#R654"
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "multipart test"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+
+    # Create the plain-text and HTML version of your message
+    text = """\
+    """
+
+    with open(inserttemplate, "r") as f:
+        contents = f.read()
+        html = BeautifulSoup(contents, 'lxml')
+
+    # Turn these into plain/html MIMEText objects
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(part1)
+    message.attach(part2)
+
+    # Create secure connection with server and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("webmail.nullphish.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
+        )
