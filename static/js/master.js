07f4f3a3bd26e3d481302f06e515af38d66b95b3
Adam Ploesch <alienwizardman@protonmail.com>
2020-02-23 13:00:09 -0800

    javascript and animation updates on main.html and public.html
    App tiles now have an area for a short description that animates with JS. Copied background animations in main.html to public.html.

---
+++ static/js/master.js
@@ -0,0 +1,8 @@
+function showAppDescription() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
