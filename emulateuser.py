95fc25c00eb9bfac799b1a68683e045c1c4a7109
Kevin <kdbrown5@gmail.com>
2020-02-29 22:55:47 -0800

    test fix

--- emulateuser.py
+++ emulateuser.py
@@ -22,7 +22,7 @@ def emulatelogin():
             cur.execute('PRAGMA key = '+dbkey+';')
             con.row_factory = sqlite.Row
             cur.execute('select username from users where business = (?);', (session['business'],))
-            reguserquery = cur.fetchall()
+            reguserquery = cur.fetchall()[0]
         con.close()
         return reguserquery
 
