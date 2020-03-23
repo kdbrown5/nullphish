2ad0916aa4c31201bb98d76e209f5b42762aa47b
Kevin <kdbrown5@gmail.com>
2020-03-22 20:03:25 -0700

    fix column for csv

--- phishingstats.py
+++ phishingstats.py
@@ -39,7 +39,7 @@ def phishingstatsload():
     def exportemail(newreport):
         with open(newreport, 'w', newline='') as f:
             writer = csv.writer(f)
-            writer.writerow(('Department', 'Method', 'User_Phished', 'Template_Used' 'Business', 'Admin_Notified', 'Date'))
+            writer.writerow(('Department', 'Method', 'User_Phished', 'Template_Used', 'Business', 'Admin_Notified', 'Date'))
             for item in emailquery:
                 writer.writerow((item[7], item[10], item[1], item[2], item[4], item[6], item[3]))
 
