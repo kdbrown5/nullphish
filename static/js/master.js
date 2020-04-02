537ab72353426efb8d4704f619ad02802e642ddc
Adam Ploesch <alienwizardman@protonmail.com>
2020-04-01 17:34:40 -0700

    actually linked js

--- static/js/master.js
+++ static/js/master.js
@@ -2,11 +2,11 @@ function checknav() {
   var w = window.innerWidth;
   var h = window.innerHeight;
   if (w < 1080) {
-    document.getElementById("menu").style.display = "none"; 
+    document.getElementById("mainmenu").style.display = "none"; 
     document.getElementById("dropdown").style.display = "inline-block"; 
   }
   else {
-    document.getElementById("menu").style.display = "table"; 
+    document.getElementById("mainmenu").style.display = "initial"; 
     document.getElementById("dropdown").style.display = "none"; 
   }
 }
