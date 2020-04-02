b5f6ce68596634b06f92d5d42f16d7997f406c23
Adam Ploesch <alienwizardman@protonmail.com>
2020-04-02 11:17:35 -0700

    implement new css for tile/list status and update placeholder

--- static/js/master.js
+++ static/js/master.js
@@ -23,12 +23,3 @@ function checkcontentontainers() {
     document.getElementById("tilelist").style.display = "none"; 
   }
 }
-
-function toggledropdown() {
-  if (document.getElementById("dropcontent").style.display = "none") {
-    document.getElementById("dropcontent").style.display = "block";
-  }
-  else {
-    document.getElementById("dropcontent").style.display = "none";
-  }
-}
