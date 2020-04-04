734b428b101712eedc617a5656744ef249781a35
Adam Ploesch <alienwizardman@protonmail.com>
2020-04-04 12:43:04 -0700

    test privacy and cookie policy display

--- static/js/master.js
+++ static/js/master.js
@@ -4,8 +4,7 @@ function checknav() {
   if (w < 1200) {
     document.getElementById("mainmenu").style.display = "none"; 
     document.getElementById("dropdown").style.display = "block"; 
-  }
-  else {
+  } else {
     document.getElementById("mainmenu").style.display = "initial"; 
     document.getElementById("dropdown").style.display = "none"; 
   }
@@ -17,9 +16,20 @@ function checkcontentontainers() {
   if (w < 1080) {
     document.getElementById("tilecontainer").style.display = "none"; 
     document.getElementById("tilelist").style.display = "block"; 
-  }
-  else {
+  } else {
     document.getElementById("tilecontainer").style.display = "grid"; 
     document.getElementById("tilelist").style.display = "none"; 
   }
+}
+
+function showpolicy() {
+  var accepted = getCookie("acceptedpolicy");
+  if (accepted = "") {
+    document.getElementById("policycontainer").style.display = "block"; 
+  }
+}
+
+function acceptpolicy() {
+  document.cookie = "acceptedpolicy=True"; 
+  document.getElementById("policycontainer").style.display = "block"; 
 }
