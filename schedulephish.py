92caf2adda17781c75d436c6e61459a3b9f22f71
Kevin <kdbrown5@gmail.com>
2020-04-05 22:31:37 -0700

    debugg

--- schedulephish.py
+++ schedulephish.py
@@ -72,15 +72,15 @@ def checkschedule():
                 zlink = [zlink]
                 zlink = zlink[0]
                 customsendphish(zsender, ztemplate, zemail, zfirstname, zlastname, zsubject, zlink, zbusiness)
-                cur.execute('update schedule set scheduled = 1 where id = (?);', (list(zid,)))
-                cur.execute('update schedule set sent = (?) where id = (?);', (timestamp,), (list(zid,)))
+                cur.execute('update schedule set scheduled = 1 where id = (?);', (zid))
+                cur.execute('update schedule set sent = (?) where id = (?);', (timestamp,), (zid))
             else:
                 zlink = 'https://app.nullphish.com/fy?id='+ztoken+'&template='+(str(ztemplate))
                 zlink = [zlink]
                 zlink = zlink[0]
                 customsendphish(zsender, ztemplate, zemail, zfirstname, zlastname, zsubject, zlink, zbusiness)
-                cur.execute('update schedule set scheduled = 1 where id = (?);', (list(zid,)))
-                cur.execute('update schedule set sent = (?) where id = (?);', (timestamp,), (list(zid,)))        
+                cur.execute('update schedule set scheduled = 1 where id = (?);', (zid))
+                cur.execute('update schedule set sent = (?) where id = (?);', (timestamp,), (zid))        
 
 #cur.execute(insert into schedule (type, username, template, mailname, date) values ('email', 'kdbrown5@gmail.com', 'Refund', 'donotreply@transactiondetails.com', '2020-04-05 17:30')
 
