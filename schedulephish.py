d65969670aa1622f2a5d9420c8f5fb25a7509321
Kevin <kdbrown5@gmail.com>
2020-04-05 20:14:56 -0700

    fix template loc

--- schedulephish.py
+++ schedulephish.py
@@ -46,7 +46,7 @@ def checkschedule():
             zbusiness = email[8]
             zbusiness = [zbusiness]
             zbusiness = zbusiness[0]
-            ztemplate = '/home/nullphish/prod/templates/businesses/'+zbusiness+'/'+ztemplate
+            ztemplate = '/home/nullphish/prod/templates/businesses/'+zbusiness+'/'+ztemplate+'.html'
             zsubject = email[9]
             cur.execute('select firstname from users where username = (?);', (zemail,))
             zfirstname = cur.fetchall()
