fce3d419db5d97dd2a75c71bcafdabdbffb74671
Kevin <kdbrown5@gmail.com>
2020-04-10 17:26:12 -0700

    debugging

--- schedulephish.py
+++ schedulephish.py
@@ -174,8 +174,9 @@ def phishschedule():
                 print(g2, g3, g4, g5, g6, g7, g8)
                 newtoken = generate_confirmation_token(g4)
                 subject = lookupemailsubject(g5)
+                print('sub')
                 print(subject)
-                print(subject[0])
+                print('end sub')    
                 if g7 == "short":
                     link = 'https://app.nullphish.com/fy?id='+newtoken+'&template='+(str(templatename))
                     link = linkshorten(link)
