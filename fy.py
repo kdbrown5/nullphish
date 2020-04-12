81bfd2b1510d12cc823e8b167cf4272350a34d11
Kevin <kdbrown5@gmail.com>
2020-04-12 11:58:13 -0700

    test fix

--- fy.py
+++ fy.py
@@ -85,8 +85,7 @@ def apiid():
                 business = cur.fetchone()[0]
                 cur.execute('update phished set business = (?) where date = (?);', (business, timestamp,))
                 cur.execute('select username from users where notify = 1 and business = (?);', (business,))
-                admins = cur.fetchall()
-                admins = admins[0]
+                admins = cur.fetchone()
                 cur.execute('update phished set admin = (?) where date = (?);', (admins, timestamp,))
                 cur.execute('select department from users where username = (?);', (email,))
                 userdept = cur.fetchone()[0]
