282f37eab8f084296f40a292f3c5ffd9176f6254
Kevin <kdbrown5@gmail.com>
2020-04-23 21:53:40 -0700

    prototype

--- testingd.py
+++ testingd.py
@@ -18,16 +18,19 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app = dash.Dash()
 
 def templatestats():
-    business = 'nullphish'
-    template = 'Lyft'
+    business = session['business']
     con = sqlite.connect('db/db1.db')
     con.row_factory = dict_factory
     cur = con.cursor()
     cur.execute('PRAGMA key = '+dbkey+';')
-    cur.execute('select * from phishsched where business = (?);', (business,))
+    cur.execute('select * from phishsched where business = (?) and activetime != "none";', (business,))
     tempstats = cur.fetchall()
+    cur.execute('select * from phishsched where business = (?) and activetime = "none";', (business,))
+    notphished = cur.fetchall()
+    cur.execute('select * from phishsched where business = (?) and sentdate != "none";', (business,))
+    sentline = cur.fetchall()
     con.close()
-    return tempstats
+    return tempstats, notphished, sentline
 
 def dict_factory(cursor, row):
     d = {}
@@ -35,17 +38,67 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d    
 
-tempstats = templatestats()
+tempstats, notphished, sentline = templatestats()
 tempx = []
 tempy = []
+notphishedx = []
+notphishedy = []
+totalopened = 0
+totalunopened = 0
+
+linephish = tempstats
+linenophish = notphished
+
+xa = []
+xb = []
+xc = 0
+xd = []
+#for i in sentline:
+#    for k, v in i.items():
+#        if k == 'date'
+#    print(i)
+
+
+for i in sentline:
+    for k, v in i.items():
+        if k == 'sentdate':
+            xa.append(v)
+        if k == 'activetime':
+            if v == "none":
+                print('activetime-v', v)
+                xb.append(-1)
+                xc = xc-1
+                xd.append(xc)
+            if v != "none":
+                xb.append(1)
+                xc = xc+1
+                xd.append(xc)
+
+print('xa', xa)
+print('xb', xb)
+
 for i in tempstats:
+    totalopened = totalopened+1
     for k, v in i.items():
         if k == 'template':
             tempy.append(1)
             tempx.append(v)
+            
+for i in notphished:
+    totalunopened = totalunopened+1
+    for k, v in i.items():
+        if k == 'template':
+            notphishedy.append(1)
+            notphishedx.append(v)
 
-print('tempx',tempx)
-print('tempy',tempy)
+totalsent = totalopened+totalunopened
+#print('tempx',tempx)
+#print('tempy',tempy)
+#print('totalsent', totalsent)
+#print('totalopened', totalopened)
+#print('totalunopened', totalunopened)
+
+
 
 
 colors = {
@@ -54,13 +107,13 @@ colors = {
 }
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H1(
-        children='Hello Dash',
+        children='Templates sent',
         style={
             'textAlign': 'center',
             'color': colors['text']
         }
     ),
-    html.Div(children='Dash: A web application framework for Python.', style={
+    html.Div(children='---', style={
         'textAlign': 'center',
         'color': colors['text']
     }),
@@ -68,8 +121,27 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         id='Graph1',
         figure={
             'data': [
-                {'x': tempx, 'y': tempy, 'type': 'bar', 'name': 'Templates sent'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                {'x': 'Total Sent', 'y': [totalsent], 'type': 'bar', 'name': 'Total Sent'},
+                {'x': 'Total Opened', 'y': [totalopened], 'type': 'bar', 'name': 'Total Opened'},
+                {'x': 'Total Unopened', 'y': [totalunopened], 'type': 'bar', 'name': 'Total Unopened'},
+                {'x': tempx, 'y': tempy, 'type': 'bar', 'name': 'Opened'},
+                {'x': notphishedx, 'y': notphishedy, 'type': 'bar', 'name': u'Not opened'},
+            ],
+            'layout': {
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
+            }
+        }
+    ),
+    dcc.Graph(
+        id='Graph2',
+        figure={
+            'data': [
+                {'x': xa, 'y': xb, 'type': 'bar', 'name': 'Opened'},
+                {'x': xa, 'y': xd, 'type': 'line', 'name': 'Opened'},
             ],
             'layout': {
                 'plot_bgcolor': colors['background'],
@@ -80,6 +152,8 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
             }
         }
     )
+
+
 ])
 
 if __name__ == '__main__':
