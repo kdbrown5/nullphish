871e3d88c18167464a2280eee648ed27632f54c6
Kevin <kdbrown5@gmail.com>
2020-04-26 22:08:25 -0700

    debugging

--- app1.py
+++ app1.py
@@ -284,6 +284,15 @@ def make_layout():
     xa, xb, xc, xd, xe, notphishedx, notphishedy, xavglist = makecounters(sentline, notphished)
     tempx, tempy = mutatetempstats(tempstats)
     templatelist, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a0g, a1g, a2g, a3g, a4g, a5g, a6g, a7g, a8g, a9g, a10g = listtemplate()
+    print('a0 -', a0,)
+    print('a1 -', a1)
+    print('a2 -', a2)
+    print('a3 -', a3)
+    print('a4 -', a4)
+    print('a5 -', a5)
+    print('a6 -', a6)
+    print('a7 -', a7)
+    print('a8 -', a8)
     colors = {
         'background': '#111111',
         'text': '#7FDBFF'
